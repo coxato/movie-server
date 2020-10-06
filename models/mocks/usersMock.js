a83817064687483bbcb5ff6019d0d593e863922b
Carlos Martinez <edu6jas@gmail.com>
2020-10-05 23:31:38 -0400

    encrypt mock user password

--- models/mocks/usersMock.js
+++ models/mocks/usersMock.js
@@ -1,12 +1,13 @@
 const User = require('../user');
 const md5 = require("md5");
+const bcrypt = require("bcryptjs");
 const usersMock = [];
 
 for (let i = 1; i <= 10; i++) {
     // basic data
     const username = `username${i}`,
     email = `user${i}@mail.com`,
-    password = 'password123',
+    password =  bcrypt.hashSync('password123', 10),
     photoUrl = `https://www.gravatar.com/avatar/${md5(email)}?d=identicon`, 
     user = new User({
         username,
